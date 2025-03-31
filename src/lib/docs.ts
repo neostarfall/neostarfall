7a5928351d6fe5c1c808d7eed1a80a2522da46c2
Vurv <vurvdevelops@gmail.com>
2025-03-30 21:57:23 -0700

    Refactor global doc storage
    This should properly refresh react components depending on the docs when the local docs injected change (now using window.globalDocManager.setDocs), at least I assume that's what the issue is, because I can't actually debug with a local custom build because of the restriction on localhost, and I can't be arsed to set up something else.

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -47,34 +47,71 @@ export type SFDocs = {
 
 // This is stored on the window so DHTML can inject the local json through here.
 declare namespace window {
-	let CachedDocs: SFDocs | null;
+	let globalDocManager: GlobalDocManager;
 }
 
-export async function getSFDocs(): Promise<SFDocs> {
-	if (window.CachedDocs) {
-		return window.CachedDocs;
-	}
-
+export async function fetchSFDocs(): Promise<SFDocs> {
 	const docs = await fetch(
 		"https://raw.githubusercontent.com/neostarfall/neostarfall/refs/heads/gh-pages/sf_doc.json",
 	);
-	const docsJson = await docs.json();
 
-	window.CachedDocs = docsJson as SFDocs;
-
-	return window.CachedDocs;
+	return await docs.json();
 }
+
+class GlobalDocManager {
+	private docs: SFDocs | null = null;
+	private listeners: Array<(docs: SFDocs) => void> = [];
+
+	setDocs(docs: SFDocs) {
+		this.docs = docs;
+
+		for (const listener of this.listeners) {
+			listener(docs);
+		}
+	}
+
+	hasDocs(): boolean {
+		return this.docs !== null;
+	}
+
+	getDocs(): SFDocs | null {
+		return this.docs;
+	}
+
+	addListener(listener: (docs: SFDocs) => void) {
+		if (this.docs) {
+			listener(this.docs);
+		} else {
+			this.listeners.push(listener);
+		}
+	}
+
+	removeListener(listener: (docs: SFDocs) => void) {
+		const index = this.listeners.indexOf(listener);
+		if (index !== -1) {
+			this.listeners.splice(index, 1);
+		}
+	}
+}
+
+window.globalDocManager = new GlobalDocManager();
 
 export function useDocs() {
 	const [docs, setDocs] = useState<SFDocs | null>(null);
 
 	useEffect(() => {
-		const fetchDocs = async () => {
-			const docs = await getSFDocs();
-			setDocs(docs);
-		};
+		const onUpdate = (docs: SFDocs) => setDocs(docs);
+		window.globalDocManager.addListener(onUpdate);
 
-		fetchDocs();
+		if (!window.globalDocManager.hasDocs()) {
+			fetchSFDocs().then((fetchedDocs) => {
+				window.globalDocManager.setDocs(fetchedDocs);
+			});
+		}
+
+		return () => {
+			window.globalDocManager.removeListener(onUpdate);
+		};
 	}, []);
 
 	return docs;
