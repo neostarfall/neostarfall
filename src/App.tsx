4cc0a36c1dee4a1ad6cdbe4257dad975aaa0594d
Vurv <vurvdevelops@gmail.com>
2025-04-21 12:36:52 -0700

    Don't use dynamic viewport units
    Hopefully should fix it entirely for the chrome supported by gmod's CEF branch.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { fetchSFDocs, globalDocManager } from "./lib/docs";
 
 function App() {
 	return (
-		<div className="flex flex-row w-dvw h-dvh overflow-hidden">
+		<div className="flex flex-row size-full h-[100vh] overflow-hidden">
 			<LeftPanel className="min-w-40 lg:min-w-80 resize-x overflow-x-auto h-full border-r border-white" />
 			<RightPanel className="h-full" />
 		</div>
